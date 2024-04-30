f1744aecc028b6e5e1e88ff6d21f053d518b4298
EmamH <heba.emam@vodafone.com.eg>
2024-05-01 01:22:37 +0300

    Read Me

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# AutomationProject
+# Mobile Automation Project
+
+# Overview
+This project aims to automate the testing of mobile applications using Java, Selenium WebDriver, Cucumber and Appium. using Behavior-Driven Development (BDD) methodology By leveraging these technologies, we can create automated test scripts to verify the functionality and behavior of mobile applications across different platforms and devices.
+
+# Prerequisites
+Before running the automated tests, ensure that the following prerequisites are met:
+
+Install Java Development Kit (JDK). You can download JDK from Oracle's website.
+Set up Java environment variables (JAVA_HOME and PATH) on your system.
+Install Maven. You can download Maven from Apache Maven's website.
+Install an Integrated Development Environment (IDE) such as IntelliJ IDEA or Eclipse for Java development.
+Download and install Appium Server. You can download Appium from Appium's website.
+Install Android SDK and configure Android Virtual Device (AVD) or connect a physical Android device for testing.
+Install necessary dependencies in your project's pom.xml file, including Selenium WebDriver, Cucumber, and Appium Java client.
+Getting Started
+To get started with running the automated tests, follow these steps:
+
+Clone this repository to your local machine:
+bash
+Copy code
+git clone https://github.com/Heba13/Mobile-App-AutomationProject
+Set capabilities for the target 
+# Project structure
+
+-src/test/java/Sample/tests :  contains tests and it's implementation 
+-src/test/java/Sample/utilities : contains report classes 
+-src/test/java/Sample/tests/BaseTest.java  : contains Configure test environment settings such as device capabilities, URLs, and other configurations .
+-pom.xml: Maven project configuration file specifying dependencies and build settings.
+# Steps
+- Start the Appium Server.
+- Run the automated tests from TestNG file 
+- Monitor the test execution in the console. Upon completion, a detailed test report will be generated in "reports"
--- src/test/java/Sample/ExtentReport_JunitDemo.java
+++ src/test/java/Sample/ExtentReport_JunitDemo.java
@@ -1,42 +1,47 @@
-package Sample;
-
-import com.relevantcodes.extentreports.ExtentReports;
-import com.relevantcodes.extentreports.ExtentTest;
-import com.relevantcodes.extentreports.LogStatus;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-
-
- public class ExtentReport_JunitDemo {
-    static ExtentTest test;
-    static ExtentReports report;
-    @BeforeClass
-    public static void startTest()
-    {
-        report = new ExtentReports("src/test/java/Sample/report/report.html");
-        test = report.startTest("ExtentDemo");
-    }
-    @Test
-    public void extentReportsDemo()
-    {
-        System.setProperty("webdriver.edge.driver","D://driver.exe");
-        WebDriver driver = new EdgeDriver();
-        driver.get("https://www.google.co.in");
-        if(driver.getTitle().equals("Google"))
-        {
-            test.log(LogStatus.PASS, "Navigated to the specified URL");
-        }
-        else
-        {
-            test.log(LogStatus.FAIL, "Test Failed");
-        }}
-    @AfterClass
-    public static void endTest()
-    {
-        report.endTest(test);
-        report.flush();
-    }}
-
+//package Sample;
+//
+//import com.aventstack.extentreports.ExtentReports;
+//import com.aventstack.extentreports.ExtentTest;
+//import com.relevantcodes.extentreports.ExtentReports;
+//import com.relevantcodes.extentreports.ExtentTest;
+//import com.relevantcodes.extentreports.LogStatus;
+//import org.junit.AfterClass;
+//import org.junit.BeforeClass;
+//import org.junit.Test;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.edge.EdgeDriver;
+//import org.testng.annotations.AfterClass;
+//import org.testng.annotations.BeforeClass;
+//import org.testng.annotations.Test;
+//
+//
+//public class ExtentReport_JunitDemo {
+//    static ExtentTest test;
+//    static ExtentReports report;
+//    @BeforeClass
+//    public static void startTest()
+//    {
+//        report = new ExtentReports();
+//        test = report.createTest("ExtentDemo");
+//    }
+//    @Test
+//    public void extentReportsDemo()
+//    {
+//        System.setProperty("webdriver.edge.driver","D://driver.exe");
+//        WebDriver driver = new EdgeDriver();
+//        driver.get("https://www.google.co.in");
+//        if(driver.getTitle().equals("Google"))
+//        {
+//            test.log(LogStatus.PASS, "Navigated to the specified URL");
+//        }
+//        else
+//        {
+//            test.log(LogStatus.FAIL, "Test Failed");
+//        }}
+//    @AfterClass
+//    public static void endTest()
+//    {
+//        report.endTest(test);
+//        report.flush();
+//    }}
+//
